c0a2c62ee8bf5c886ad0908ea1505e09e20398e4
Viny <viniciusmeireles@gmail.com>
2022-01-27 15:56:30 -0300

    Projeto Final da Aula ReactJS Ocean

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Calendario from "./components/Calendario/Calendario";
-import Calendar from 'react-calendar';
+import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import {Button, Spinner} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <div>
-  <p><Calendario /></p>
-  <p><Calendar /></p>
+    <Button variant="outline-success">Olá</Button>
+    <p></p>
+    <Spinner animation="grow" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+    <p></p>
+    <Calendario />  
+    <p></p>
+    <Calendar calendarType="US" />
   </div>
   ,
   document.getElementById('root')
